015b20796f2cddd591b2c944eac6164d35a81475
haNi <2096420073@qq.com>
2023-09-05 20:28:42 +0800

    项目初始化

---
+++ src/store/getters.js
@@ -0,0 +1,10 @@
+const getters = {
+  sidebar: state => state.app.sidebar, // 取app模块属性
+  device: state => state.app.device,
+  token: state => state.user.token, // 取user模块属性
+  userId: state => state.user.userInfo.userId,
+  avatar: state => state.user.avatar,
+  name: state => state.user.name
+}
+// getters便捷访问
+export default getters
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <!-- 使用svg-icon组件 -->
+    <!-- 设置iconClass属性 = "文件名(没有后缀)" -->
+    <svg-icon icon-class="dashboard" />
+    <svg-icon icon-class="example" />
+    <svg-icon icon-class="password" />
+    <svg-icon icon-class="skill" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  },
+  created() {
+    // 初始化执行
+    this.$store.dispatch('user/getUserInfo')
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
