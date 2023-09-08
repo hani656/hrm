6c09f98784f3b20b6f4dd97ad99aa6a8e974ea8d
haNi <2096420073@qq.com>
2023-09-08 14:23:23 +0800

    添加子部门组件创建

--- src/utils/index.js
+++ src/utils/index.js
@@ -125,10 +125,10 @@ export function transListToTreeData(list, rootValue) {
   const arr = []
   list.forEach(item => {
     if (item.pid === rootValue) {
-      // 找到了匹配节点
+      // 找到了匹配的节点
       arr.push(item)
-      // 当前节点的id 和 当前节点的子节点的id是相等的
-      const children = transListToTreeData(list, item.id) // 找到节点的子节点
+      // 当前节点的id 和 当前节点的子节点的pid是想等的
+      const children = transListToTreeData(list, item.id) // 找到的节点的子节点
       item.children = children // 将子节点赋值给当前节点
     }
   })
