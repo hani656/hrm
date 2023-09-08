895a33aad774da15ed5757fcd31558d3d2ef7817
haNi <2096420073@qq.com>
2023-09-08 15:26:58 +0800

    添加部门表单的校验

--- src/views/department/components/add-dept.vue
+++ src/views/department/components/add-dept.vue
@@ -1,18 +1,18 @@
 <template>
   <el-dialog title="新增部门" :visible="showDialog" @close="close">
     <!-- 放置弹层内容 -->
-    <el-form label-width="120px">
-      <el-form-item label="部门名称" size="mini">
-        <el-input placeholder="2-10个字符" style="width: 80%" size="mini" />
+    <el-form ref="addDept" :model="formData" :rules="rules" label-width="120px">
+      <el-form-item prop="name" label="部门名称" size="mini">
+        <el-input v-model="formData.name" placeholder="2-10个字符" style="width: 80%" size="mini" />
       </el-form-item>
-      <el-form-item label="部门编码">
-        <el-input placeholder="2-10个字符" style="width: 80%" size="mini" />
+      <el-form-item prop="code" label="部门编码">
+        <el-input v-model="formData.code" placeholder="2-10个字符" style="width: 80%" size="mini" />
       </el-form-item>
-      <el-form-item label="部门负责人">
-        <el-select placeholder="请选择负责人" style="width: 80%" size="mini" />
+      <el-form-item prop="managerId" label="部门负责人">
+        <el-select v-model="formData.managerId" placeholder="请选择负责人" style="width: 80%" size="mini" />
       </el-form-item>
-      <el-form-item label="部门介绍">
-        <el-input placeholder="1-100个字符" type="textarea" size="mini" style="width: 80%" />
+      <el-form-item prop="introduce" label="部门介绍">
+        <el-input v-model="formData.introduce" placeholder="1-100个字符" type="textarea" size="mini" style="width: 80%" />
       </el-form-item>
       <el-form-item>
         <!-- 按钮 -->
@@ -28,11 +28,68 @@
 </template>
 
 <script>
+import { getDepartment } from '@/api/dapartment'
 export default {
   props: {
     showDialog: {
       type: Boolean,
       default: false
+    }
+  },
+  data() {
+    return {
+      formData: {
+        code: '', // 部门编码
+        introduce: '', // 部门介绍
+        managerId: '', // 部门负责人名字
+        name: '', // 部门名称
+        pid: ''// 父级部门id
+      },
+      rules: {
+        code: [{ required: true, message: '部门编码不能为空', trigger: 'blur' },
+          {
+            min: 2, max: 10, message: '部门编码的长度为2-10个字符', trigger: 'blur'
+          }, {
+            trigger: 'blur',
+            // 自定义校验模式
+            validator: async(rule, value, callback) => {
+              // value就是输入的编码
+              const result = await getDepartment()
+              // result数组中是否存在 value值
+              if (result.some(item => item.code === value)) {
+                callback(new Error('部门中已经有有该编码了'))
+              } else {
+                callback()
+              }
+            }
+          }
+        ], // 部门编码
+        introduce: [{ required: true, message: '部门介绍不能为空', trigger: 'blur' },
+          {
+            min: 1, max: 100, message: '部门介绍的长度为1-100个字符', trigger: 'blur'
+          }
+        ], // 部门介绍
+        managerId: [{ required: true, message: '部门负责人不能为空', trigger: 'blur' }], // 部门负责人名字
+        name: [{ required: true, message: '部门名称不能为空', trigger: 'blur' },
+          {
+            min: 2, max: 10, message: '部门名称的长度为2-10个字符', trigger: 'blur'
+          }, {
+            trigger: 'blur',
+            // 自定义校验模式
+            validator: async(rule, value, callback) => {
+              // value就是输入的编码
+              const result = await getDepartment()
+              // result数组中是否存在 value值
+              if (result.some(item => item.name === value)) {
+                callback(new Error('部门中已经有有该名称了'))
+              } else {
+                callback()
+              }
+            }
+          }
+        ] // 部门名称
+        // pid: ''// 父级部门id 不需要做校验
+      }
     }
   },
   methods: {
