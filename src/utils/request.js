e24aed62e6d4a0d264577921bd908514ec9e365b
haNi <2096420073@qq.com>
2023-09-11 14:18:48 +0800

    员工的excel导出

--- src/utils/request.js
+++ src/utils/request.js
@@ -26,6 +26,8 @@ service.interceptors.request.use((config) => {
 // 响应拦截器
 service.interceptors.response.use((response) => {
   // axios默认包裹了data
+  // 判断是不是Blob
+  if (response.data instanceof Blob) return response.data // 返回了Blob对象
   const { data, message, success } = response.data
   if (success) {
     return data
