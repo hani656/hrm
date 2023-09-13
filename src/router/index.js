88673e7163f9a6901035a1483b3349fd89507e6a
haNi <2096420073@qq.com>
2023-09-13 19:45:03 +0800

    将路由改成history模式

--- src/router/index.js
+++ src/router/index.js
@@ -78,7 +78,7 @@ export const asyncRoutes = [
   socialRouter
 ]
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes // 默认引入静态路由
 })
